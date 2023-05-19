9930985f15d56d06accd9c06f9f1fd1e2b5b80b5
Israel <israelrodriguesmorais@hotmail.com>
2023-05-19 20:45:31 -0300

    feat(project): add response dto

--- src/dtos/validation/LoginValidationDTO.ts
+++ src/dtos/validation/LoginValidationDTO.ts
@@ -1,11 +1,10 @@
-import { JoiSchemaOptions, JoiSchema } from "nestjs-joi";
-import { AuthenticationSchema } from "src/schemas/AuthenticationSchema";
+import { JoiSchemaOptions, JoiSchema } from 'nestjs-joi'
+import { AuthenticationSchema } from 'src/schemas/AuthenticationSchema'
 
-
-@JoiSchemaOptions({allowUnknown:false})
-export class LoginValidationDTO{
-    @JoiSchema(AuthenticationSchema.userName.required())
-    username: string
-    @JoiSchema(AuthenticationSchema.password.required())
-    password: string
+@JoiSchemaOptions({ allowUnknown: false })
+export class LoginValidationDTO {
+  @JoiSchema(AuthenticationSchema.userName.required())
+  username: string
+  @JoiSchema(AuthenticationSchema.password.required())
+  password: string
 }
