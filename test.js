15236304bdcda805abfcb13f54f18f1fbf2a0a3f
Israel Morais <ibrmorais@viptech.com.br>
2023-05-25 20:19:50 -0300

    chore: remove unused file

--- test.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios')
-
-axios
-  .post('http://localhost:3000/login', {
-    username: 'teste',
-    password: '123',
-  })
-  .then((response) => console.log(response.data))
